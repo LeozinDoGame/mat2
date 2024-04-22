61122a3d05308e1027e946d6702fef7c222ed2c5
profehigor <165037678+profehigor@users.noreply.github.com>
2024-04-22 08:56:36 -0300

    aiai

--- main.js
+++ main.js
@@ -14,11 +14,12 @@ for(let i=0;i <botoes.length;i++){
     }
 }
 
-const contadores = document.querySelectorAll(".contador")
-const tempoObjetivo1 = new Date("2024-02-05T00:00:00")
-const tempoObjetivo2 = new Date("2024-10-10T00:00:00")
-const tempoObjetivo3 = new Date("2024-10-25T00:00:00")
-const tempoObjetivo4 = new Date("2024-10-19T00:00:00")
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-02-05T00:00:00");
+const tempoObjetivo2 = new Date("2024-10-10T00:00:00");
+const tempoObjetivo3 = new Date("2024-10-25T00:00:00");
+const tempoObjetivo4 = new Date("2024-10-19T00:00:00");
+
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
 contadores[0].textContent = calculaTempo(tempos[0]);
@@ -38,19 +39,15 @@ function atualizaCronometro(){
 function comecaCronometro(){
     atualizaCronometro();
     setInterval(atualizaCronometro,1000); }
-    comecaCronometro();
-
-atualizaCronometro();
-setInterval(atualizaCronometro, 1000);
-
-function calculaTempo(tempoObjetivo){
+    
+    function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
     let segundos = Math.floor(tempoFinal/1000);
     let minutos = Math.floor(segundos/60);
     let horas = Math.floor(minutos/60);
     let dias = Math.floor(horas/24)
-
+    
     segundos %=60;
     minutos %=60;
     horas %=24;
@@ -60,10 +57,11 @@ function calculaTempo(tempoObjetivo){
 }
 
 if (tempoFinal > 0){
-    return dias + “ dias “ + horas + “ horas “ + minutos + “
-    minutos “ + segundos + “ segundos”;
-    } 
+    return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
+} 
     
     else {
-    return “Prazo Finalizado”;
+    return "Prazo Finalizado";
     }
+    
+comecaCronometro();
